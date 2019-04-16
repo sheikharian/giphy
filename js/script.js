7936374a4e85bb3cbb6ddd27e16f111458967353
sheikharian <sheikha2430@queenstechhs.org>
2019-04-16 17:53:09 +0000

    added ajax

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
+    $.ajax({url:"https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC", success: function(result){
+        $("body").html(images.original_still);
+    }})
+
+
 });
 
